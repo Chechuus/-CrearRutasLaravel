7a0c6ae2ecefa2e445b6bf9fdb98062763615d2c
Cecilia Ceballos <ceciliaceb@gmail.com>
2022-11-20 23:19:41 -0300

    Crear Rutas Laravel

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function () {
+    return "hola";
+});
+Route::get('rama1/rama2', function () {
+    return "con una rama ";
+});
+Route::get('cursos/{cursito}', function ($cursito) {
+    return "Esta es una variable: $cursito";
+});
+Route::get('cursos/{cursito}/{otrocurso}', function ($cursito, $otrocurso) {
+    return "Esta es una variable: $cursito , y esta es otra variable $otrocurso";
+});
